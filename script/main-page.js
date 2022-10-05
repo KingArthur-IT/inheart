693ed0c39242e9c0113a4f84dd92c2662041f1aa
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-10-05 21:45:58 +0300

    color themes, head color and head animations

--- script/main-page.js
+++ script/main-page.js
@@ -1,7 +1,45 @@
+//set theme color
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
+const theme = urlParams.get('theme');
+
+let themeHslColor = {h: 176, s: 93, l: 6}
+switch (theme) {
+  case 'black':
+    themeHslColor = {h: 180, s: 3, l: 8}
+    break;
+  case 'red':
+    themeHslColor = {h: 349, s: 85, l: 11}
+    break;
+  case 'blue':
+    themeHslColor = {h: 220, s: 87, l: 17}
+    break; 
+
+  default:
+    break;
+}
+
 if (urlParams.has('theme')){
-  document.querySelector('body').classList.add(urlParams.get('theme'))
+  document.querySelector('body')?.classList.add(theme)
+}
+
+const headElement = document.querySelector('.page-head');
+if (headElement){
+  const winWidth = window.innerWidth;
+  const winHeight = window.innerHeight;
+
+  headElement.addEventListener('mousemove', (e) => {
+    const xColorDisplacement = Math.round(20.0 * e.clientX / winWidth - 1.0);
+    const yColorDisplacement = Math.round(4.0 * e.clientY / winHeight - 2.0);
+
+    const xPhotoDisplacement = Math.round(5 * e.clientX / winWidth - 2.5);
+    const yPhotoDisplacement = Math.round(5 * e.clientY / winHeight - 2.5);
+
+    document.querySelector('body').style["background-color"] = `hsl(${themeHslColor.h + xColorDisplacement}, ${themeHslColor.s}%, ${themeHslColor.l + yColorDisplacement}%)`;
+    document.querySelector('.page-head__thumbnail-label').style["background-color"] = `hsl(${themeHslColor.h + xColorDisplacement}, ${themeHslColor.s}%, ${themeHslColor.l + yColorDisplacement}%)`;
+
+    document.querySelector('.page-head__photo img').style['transform'] = `translate(${xPhotoDisplacement}%, ${yPhotoDisplacement}%)`
+  })
 }
 
 // Carousel on choose color temlate for tablet and mobile
