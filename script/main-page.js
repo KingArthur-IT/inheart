908f3691d3039e8c966fcc0f2a74054d23a62ad6
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-10-10 14:18:55 +0300

    test

--- script/main-page.js
+++ script/main-page.js
@@ -149,7 +149,7 @@ $(document).ready(function(){
 
   document.querySelector('.page-biography__carousel').addEventListener('wheel', (e) => {
     e.preventDefault();
-    console.log(s);
+    console.log(s, e.wheelDeltaY);
     const sign = e.wheelDeltaY < 0 ? -1 : 1;
     if ((sign === -1 && s > 400) || (sign === 1 && s <= 3000)){
       s += sign * 100;
